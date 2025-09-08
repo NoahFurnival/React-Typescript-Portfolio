e18b7db74ef2f789054f744343aff0580eb55a0c
Noah Furnival <noahfurnival@gmail.com>
2025-09-08 10:38:33 -0400

    added ilisten to projects

--- src/shared/projectConstants.ts
+++ src/shared/projectConstants.ts
@@ -1,6 +1,7 @@
 import { type Project } from "@/components/ProjectCard";
 import blog from "@/assets/blog.png";
 import vl from "@/assets/vl.png";
+import ilisten from "@/assets/ilisten.png";
 import chatbot from "@/assets/chatbot.png";
 export const projects: Project[] = [
   {
@@ -10,6 +11,15 @@ export const projects: Project[] = [
     description:
       "A clean website built in react and tailwind that show cases VisionLeads services. The site includes a clear call-to-action encouraging visitors to request a free ICP (Ideal Customer Persona) assessment.",
     link: "https://visionleads.io/",
+    github: false,
+  },
+  {
+    picture: ilisten,
+    title: "iListen Website",
+    softwares: ["Wordpress"],
+    description:
+      "iListen Canada is a friendly, mobile-first website designed to make booking a listening session feel simple, not stressful. The homepage guides you with clear calls to action and a streamlined “Book Now” path that works just as well on a phone as on a laptop.",
+    link: "https://ilistencanada.com",
     github: false,
   },
   {
